c1f5e18f91d3afdf38f84e1e7da05182d98a95c5
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2024-05-28 12:38:34 -0400

    Remove unused fetch functions

--- app/library/opens/page.tsx
+++ app/library/opens/page.tsx
@@ -1,5 +1,4 @@
 import { InsightCard, TrackInsightCard } from "@/components/InsightCard";
-import { fetchTracks } from "@/lib/data";
 import { Suspense } from "react";
 
 interface LibraryProps {
@@ -9,15 +8,9 @@ interface LibraryProps {
 }
 
 export default async function Library({ params }: LibraryProps) {
-  const tracks = await fetchTracks();
-
   return (
     <div className="flex flex-col gap-4 mb-20">
-      <Suspense fallback={<div>TESTING</div>}>
-        {tracks.map(track => (
-          <InsightCard key={track.id} track={track} />
-        ))}
-      </Suspense>
+      <Suspense fallback={<div>TESTING</div>}>import opens</Suspense>
     </div>
   );
 }
